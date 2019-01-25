85bdb2faf4444fd3b83ece809cb555b72fa41dfd
Yuval Steuer <yuvalsteuer11@gmail.com>
2019-01-25 13:11:52 +0200

    Fixed

--- Project/cvWebApplication/App_Data/dbo.Users.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Users] (
-    [MemberId]      INT  NOT NULL,
-    [Username]      TEXT NOT NULL,
-    [Password]		VARCHAR(16) NOT NULL ,
-    PRIMARY KEY		CLUSTERED ([MemberId] ASC)
-);
-
