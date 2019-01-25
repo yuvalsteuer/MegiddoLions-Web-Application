d17310e4db466b6f85c328eaa780ab9cb2a7b3da
Yuval Steuer <yuvalsteuer11@gmail.com>
2019-01-25 16:28:29 +0200

    Roee Project

---
+++ ROEE_LOOK_HERE/cvWebApplication/cvWebApplication/Models/Project.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace cvWebApplication.Models
+{
+	public class Project
+	{
+		private static int id = 0;
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Url { get; set; }
+
+
+		public Project(string name, string url)
+		{
+			Name = name;
+			Url = url;
+			Id = id;
+			id++;
+
+		}
+
+		
+	}
+}
