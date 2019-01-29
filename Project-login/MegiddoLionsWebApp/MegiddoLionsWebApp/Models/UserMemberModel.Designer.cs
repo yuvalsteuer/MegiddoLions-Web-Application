e1e5b73d49c311e0f8daee2e8d47bba2eb1f4a0b
Yuval Steuer <yuvalsteuer11@gmail.com>
2019-01-29 18:50:29 +0200

    Login, Register

--- Project-login/MegiddoLionsWebApp/MegiddoLionsWebApp/Models/UserMemberModel.Designer.cs
+++ Project-login/MegiddoLionsWebApp/MegiddoLionsWebApp/Models/UserMemberModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\yuval\Desktop\webRepo\Web-Application-CV\Project-login\MegiddoLionsWebApp\MegiddoLionsWebApp\Models\UserMemberModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\yuval\Desktop\webRepo\MegiddoLions-Web-Application\Project-login\MegiddoLionsWebApp\MegiddoLionsWebApp\Models\UserMemberModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
