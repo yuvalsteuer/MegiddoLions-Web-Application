812ea7ebd0eba556720ab1fd93902e422c126874
Yuval Steuer <yuvalsteuer11@gmail.com>
2019-01-29 11:13:07 +0200

    Project Name Change

---
+++ Project-login/MegiddoLionsWebApp/MegiddoLionsWebApp/Controllers/UsersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MegiddoLionsWebApp.Models;
+
+namespace MegiddoLionsWebApp.Controllers
+{
+    public class UsersController : Controller
+    {
+        private MegiddoLionsEntities db = new MegiddoLionsEntities();
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var tblUsers = db.tblUsers.Include(t => t.tblMember);
+            return View(tblUsers.ToList());
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            ViewBag.MemberID = new SelectList(db.tblMembers, "MemberID", "FirstName");
+            return View();
+        }
+
+        // POST: Users/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MemberID,Username,Password,Email")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tblUsers.Add(tblUser);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MemberID = new SelectList(db.tblMembers, "MemberID", "FirstName", tblUser.MemberID);
+            return View(tblUser);
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MemberID = new SelectList(db.tblMembers, "MemberID", "FirstName", tblUser.MemberID);
+            return View(tblUser);
+        }
+
+        // POST: Users/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MemberID,Username,Password,Email")] tblUser tblUser)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tblUser).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MemberID = new SelectList(db.tblMembers, "MemberID", "FirstName", tblUser.MemberID);
+            return View(tblUser);
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tblUser tblUser = db.tblUsers.Find(id);
+            if (tblUser == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tblUser);
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tblUser tblUser = db.tblUsers.Find(id);
+            db.tblUsers.Remove(tblUser);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Project-login/MegiddoLionsWebApp/MegiddoLionsWebApp/bin/Release/Publish/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MegiddoLionsWebApp.MvcApplication" Language="C#" %>
